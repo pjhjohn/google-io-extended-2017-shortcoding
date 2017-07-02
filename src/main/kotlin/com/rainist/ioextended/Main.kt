f32511069e0da34aeabb47cdbc37f9ddde1393e5
pjhjohn <pjhjohn@gmail.com>
2017-07-02 17:07:13 +0900

    Solution using Recursion

--- src/main/kotlin/com/rainist/ioextended/Main.kt
+++ src/main/kotlin/com/rainist/ioextended/Main.kt
@@ -2,5 +2,11 @@ package com.rainist.ioextended
 
 
 fun solve(combinedList: List<Long>): List<Long> {
-    return emptyList()
+    if (combinedList.isEmpty()) return emptyList()
+
+    val unsolved = combinedList.toMutableList()
+    unsolved.remove(combinedList[0])
+    unsolved.remove(combinedList[0] * 4 / 3)
+
+    return listOf(combinedList[0]) + solve(unsolved)
 }
